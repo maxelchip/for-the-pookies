9d7cc54be078174d0c8ecf0df115dc169581b8ec
maxelchip <mlevin742@gmail.com>
2024-03-22 21:43:00 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the tab is fully loaded
-    if (changeInfo.status === 'complete') {
       // Inject content script into the tab
-      chrome.scripting.executeScript({
+    chrome.scripting.executeScript({
         target: {tabId: tabId},
         files: ['content.js']
-      });
-    }
-  });
+    });
+});
