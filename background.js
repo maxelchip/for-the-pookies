593766afd52ef0f50765dd8dbfae88fe3efd0756
maxelchip <mlevin742@gmail.com>
2024-03-22 21:45:27 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Check if the tab is fully loaded
-      // Inject content script into the tab
     chrome.scripting.executeScript({
         target: {tabId: tabId},
         files: ['content.js']
